1c9092c0eb7fe4ce63e649f46899639b051820bb
Darya Pogo <pogorelaja.darya@yandex.ru>
2022-11-25 06:44:25 +0300

    validation

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,20 +1,18 @@
 export class FormValidation {
   constructor(formElement, popup) {
-    this.formSelector = formElement.formSelector;
-    this.inputSelector = formElement.inputSelector;
-    this.submitButtonSelector = formElement.submitButtonSelector;
-    this.inactiveButtonClass = formElement.inactiveButtonClass;
-    this.inputErrorClass = formElement.inputErrorClass;
-    this.errorClass = formElement.errorClass;
-    this.popup = popup;
-    this.inputs = Array.from(this.popup.querySelectorAll(this.inputSelector));
-    this.formSubmitButton = this.popup.querySelector(
-      this._submitButtonSelector
-    );
+    this._formSelector = formElement.formSelector,
+    this._inputSelector = formElement.inputSelector,
+    this._submitButtonSelector = formElement.submitButtonSelector,
+    this._inactiveButtonClass = formElement.inactiveButtonClass,
+    this._inputErrorClass = formElement.inputErrorClass,
+    this._errorClass = formElement.errorClass,
+    this.popup = popup,
+    this.inputs = Array.from(this.popup.querySelectorAll(this._inputSelector));
+    this.formSubmitButton = this.popup.querySelector(this._submitButtonSelector);
   }
 
   _showInputError(inputElement, errorElement) {
-    inputElement.classList.add(this.inputErrorClass);
+    inputElement.classList.add(this._inputErrorClass);
     errorElement.textContent = inputElement.validationMessage;
   }
 
@@ -24,59 +22,68 @@ export class FormValidation {
   }
 
   _enableButton() {
-    this.formSubmitButton.classList.remove(this.inactiveButtonClass);
+    this.formSubmitButton.classList.remove(this._inactiveButtonClass);
     this.formSubmitButton.disabled = false;
   }
 
   disabledButton() {
-    this.formSubmitButton.classList.add(this.inactiveButtonClass);
+    this.formSubmitButton.classList.add(this._inactiveButtonClass);
     this.formSubmitButton.disabled = true;
   }
 
-  _chekInputValidity(inputElement, errorElement) {
+  _chekInputValidity(
+    inputElement,
+    errorElement
+  ) {
     if (inputElement.validity.valid) {
-      errorElement.classList.remove(this.errorClass);
+      errorElement.classList.remove(this._errorClass);
       this._hideInputError(inputElement, errorElement);
     } else {
-      errorElement.classList.add(this.errorClass);
+      errorElement.classList.add(this._errorClass);
       this._showInputError(inputElement, errorElement);
     }
-  }
+  };
 
-  _toggleButton(buttonState) {
+  _toggleButton (
+    buttonState
+  ) {
     if (buttonState) {
       this.disabledButton();
     } else {
       this._enableButton();
     }
-  }
+  };
 
-  _hasInvalidInputs() {
+  _hasInvalidInputs () {
     return this.inputs.some((input) => !input.validity.valid);
-  }
+  };
 
-  _handelFormSubmit(event) {
-    event.preventDefault();
-  }
+  
+_handelFormSubmit(event) {
+  event.preventDefault();
+};
 
-  _handelFormInput(event) {
-    const inputElement = event.target;
-    const errorElement = this.popup.querySelector(
-      `.input-error-${inputElement.name}`
-    );
-    const buttonState = this._hasInvalidInputs(this.inputs);
-    this._chekInputValidity(inputElement, errorElement);
-    this._toggleButton(buttonState);
-  }
-
-  enableValidation() {
-    this.popup.addEventListener("submit", (event) =>
-      this._handelFormSubmit(event)
-    );
+_handelFormInput(event) {
+  const inputElement = event.target;
+  const errorElement = this.popup.querySelector(
+    `.input-error-${inputElement.name}`
+  );
+  const buttonState = this._hasInvalidInputs(this.inputs);
+  this._chekInputValidity(inputElement, errorElement);
+  this._toggleButton(buttonState);
+};
+  
+enableValidation() {
+    this.popup.addEventListener("submit",(event) => this._handelFormSubmit(event));
     this.inputs.forEach((inputElement) => {
       inputElement.addEventListener("input", (event) =>
-        this._handelFormInput(event)
-      );
+      this._handelFormInput(event));
     });
-  }
+};
 }
+
+
+
+
+
+
